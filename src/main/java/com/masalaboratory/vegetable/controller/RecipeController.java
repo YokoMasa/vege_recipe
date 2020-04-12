39c7a3421f9c27eff1dcd9e85ebaaa761265f360
masato <masa.5tonic@gmail.com>
2020-04-12 20:03:20 +0900

    added post form

--- src/main/java/com/masalaboratory/vegetable/controller/RecipeController.java
+++ src/main/java/com/masalaboratory/vegetable/controller/RecipeController.java
@@ -2,6 +2,7 @@ package com.masalaboratory.vegetable.controller;
 
 import java.util.List;
 
+import com.masalaboratory.vegetable.controller.form.RecipeForm;
 import com.masalaboratory.vegetable.model.Ingredient;
 import com.masalaboratory.vegetable.model.Recipe;
 import com.masalaboratory.vegetable.model.RecipeProc;
@@ -10,15 +11,22 @@ import com.masalaboratory.vegetable.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/recipe")
 public class RecipeController {
 
     @Autowired
     private RecipeRepository recipeRepository;
 
-    @GetMapping("/recipe")
+    @GetMapping
     private String list(Model model) {
         List<Recipe> recipes = recipeRepository.findAll();
         model.addAttribute("test", "Recipe!!");
@@ -36,6 +44,18 @@ public class RecipeController {
         }
 
         return "recipe";
+    }
+
+    @PostMapping
+    private String create(@Validated @ModelAttribute final RecipeForm form, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            System.out.println("validation error!");
+            for (final ObjectError objectError: bindingResult.getAllErrors()) {
+                System.out.println(objectError.getDefaultMessage());
+            }
+        }
+        System.out.println(form.toString());
+        return "recipe_created";
     }
 
 }
---
+++ src/main/java/com/masalaboratory/vegetable/controller/form/RecipeForm.java
@@ -0,0 +1,85 @@
+package com.masalaboratory.vegetable.controller.form;
+
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.NonNull;
+import org.springframework.web.multipart.MultipartFile;
+
+public class RecipeForm {
+
+    @NonNull
+    @Size(max = 255)
+    private String name;
+
+    @NonNull
+    @Size(max = 511)
+    private String shortDescription;
+
+    @NonNull
+    @Size(max = 4095)
+    private String longDescription;
+
+    @Size(max = 255)
+    private String ingredientOrder;
+
+    @Size(max = 255)
+    private String recipeProcOrder;
+
+    private MultipartFile image;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShortDescription() {
+        return shortDescription;
+    }
+
+    public String getLongDescription() {
+        return longDescription;
+    }
+
+    public String getIngredientOrder() {
+        return ingredientOrder;
+    }
+
+    public String getRecipeProcOrder() {
+        return recipeProcOrder;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setShortDescription(String shortDescription) {
+        this.shortDescription = shortDescription;
+    }
+
+    public void setLongDescription(String longDescription) {
+        this.longDescription = longDescription;
+    }
+
+    public void setIngredientOrder(String ingredientOrder) {
+        this.ingredientOrder = ingredientOrder;
+    }
+
+    public void setRecipeProcOrder(String recipeProcOrder) {
+        this.recipeProcOrder = recipeProcOrder;
+    }
+
+    public MultipartFile getImage() {
+        return image;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "RecipeForm [image=" + image + ", ingredientOrder=" + ingredientOrder + ", longDescription="
+                + longDescription + ", name=" + name + ", recipeProcOrder=" + recipeProcOrder + ", shortDescription="
+                + shortDescription + "]";
+    }
+
+}
