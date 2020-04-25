1b372bf4efc8f4ca2e554e76b2a0faf1221ddfab
masato <masa.5tonic@gmail.com>
2020-04-25 20:58:21 +0900

    applied css to recipe list

--- src/main/java/com/masalaboratory/vegetable/controller/RecipeController.java
+++ src/main/java/com/masalaboratory/vegetable/controller/RecipeController.java
@@ -1,5 +1,8 @@
 package com.masalaboratory.vegetable.controller;
 
+import javax.websocket.server.PathParam;
+
+import com.fasterxml.jackson.core.sym.Name;
 import com.masalaboratory.vegetable.model.Recipe;
 import com.masalaboratory.vegetable.service.RecipeService;
 
@@ -8,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,6 +27,13 @@ public class RecipeController {
         Page<Recipe> page = recipeService.getPage(pageNum);
         model.addAttribute("page", page);
         return "recipe_list";
+    }
+
+    @GetMapping("/{id}")
+    public String get(@PathVariable(name = "id") int id, Model model) {
+        Recipe r = recipeService.getById(id);
+        model.addAttribute("recipe", r);
+        return "recipe";
     }
 
 }
